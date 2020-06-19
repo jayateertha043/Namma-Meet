44f5c5a558c0a052576deb0d46a3d88f2386ac3d
jayateertha043 <jayateertha043@gmail.com>
2020-06-19 11:34:55 +0530

    to cm

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'splash.dart';
 void main(){
-    WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-    .then((_) {
       runApp(new MyApp());
-    });
 }
 
 class MyApp extends StatelessWidget {
